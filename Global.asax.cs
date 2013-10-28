87d4c6b3daf4d8dcd5ef59435a34aeaa1e31e629
Zamir Musaev <zamir.ru@gmail.com>
2013-10-29 00:49:07 +0400

    Поменял настройки Гита и добавил логирование начала приложения

--- Global.asax.cs
+++ Global.asax.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using testProject.Infrastructure.Logging;
 
 namespace testProject
 {
@@ -37,6 +38,8 @@ namespace testProject
 
             RegisterGlobalFilters(GlobalFilters.Filters);
             RegisterRoutes(RouteTable.Routes);
+
+            DependencyResolver.Current.GetService<ILogger>().Info("Application started");
         }
     }
 }
