6134f64840d9d0a393e050f6f7cafbf5ca55f815
Zamir Musaev <zamir.ru@gmail.com>
2013-11-02 19:37:21 +0400

    Добавил jQuery виджет для прогресса

---
+++ Scripts/jqProgressWidjet.js
@@ -0,0 +1,48 @@
+﻿$.widget('custom.progressbar', {
+    options: {
+        value : 0
+    },
+    _create: function () {
+        var progress = this.options.value + '%';
+        this.element
+        .addClass('progressbar')
+        .text(progress);
+    },
+
+    value: function (value) {
+        if (typeof (value) === 'undefined' || value === undefined) {
+            return this.options.value;
+        }
+
+        this.options.value = this._constrain(value);
+        var progress = this.options.value + '%';
+        this.element.text(progress);
+    },
+
+    _constrain: function (value) {
+        if (value > 100) {
+            value = 100;
+        }
+        if (value < 0) {
+            value = 0;
+        }
+        return value;
+    },
+
+    _setOption: function (key, value) {
+        if (key === 'value') {
+            value = this._constrain(value);
+        }
+        this._super(key, value);
+    },
+
+    _setOptions: function(options){
+        this._super(options);
+        this.refresh();
+    },
+
+    refresh: function () {
+        var progress = this.options.value + '%';
+        this.element.text(progress);
+    }
+});
